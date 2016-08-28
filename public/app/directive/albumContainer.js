35773def6d5f26c3f3d1d1c0e89dd8527ba58c5f
James Lemire <webdev.jameslemire@gmail.com>
2016-08-28 12:42:13 -0600

    Styles

--- public/app/directive/albumContainer.js
+++ public/app/directive/albumContainer.js
@@ -7,6 +7,28 @@ angular
       templateUrl: './view/albumContainer.html',
       scope: {
         data: '='
+      },
+      controller: function($scope, cart, ngDialog) {
+        $scope.informAdd= function() {
+          $scope.type = "Album";
+          ngDialog.open({ template: './view/addToCart.html', className: 'ngdialog-theme-default', scope: $scope });
+        };
+
+        $scope.informAlready = function() {
+          $scope.type = "Album";
+          ngDialog.open({ template: './view/alreadyInCart.html', className: 'ngdialog-theme-default', scope: $scope });
+        };
+
+        $scope.addToCart = function(data) {
+          // console.log(data);
+          // var albumID = data.albumID;
+          var temp = cart.updateCart($scope.data, $scope.data.cartID);
+          if(temp) {
+            $scope.informAdd();
+          } else {
+            $scope.informAlready();
+          }
+        };
       }
     };
 
